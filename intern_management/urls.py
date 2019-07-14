b3025e681885fffcaf802203659e54dcbbfaa874
Luke Lighthart <Cu-Sith@gmx.com>
2019-07-14 16:34:15 -0500

    Get register page working

--- intern_management/urls.py
+++ intern_management/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.details, name='details'),
+    path('account/', views.account, name='account')
 ]
--- intern_management/views.py
+++ intern_management/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 # Create your views here.
+
+
 def details(request):
     return render(request, 'intern_management/detail_page.html')
+
+
+def account(request):
+    return render(request, 'intern_management/account.html')
