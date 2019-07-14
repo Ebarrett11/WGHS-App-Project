46c21bd244c85a6ed271bedcfce079c7d652290c
Luke Lighthart <Cu-Sith@gmx.com>
2019-07-14 14:40:18 -0500

    User registration works

--- intern_management/models.py
+++ intern_management/models.py
@@ -1,15 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+
 class InternshipLocationModel(models.Model):
     address = models.CharField(max_length=400)
-
-class StudentModel(models.Model):
-    fname = models.CharField(max_length=200)
-    lname = models.CharField(max_length=200)
-
-    def full_name(self):
-        return "{} {}".format(self.fname, self.lname)
-
-    def __str__(self):
-        return self.full_name()
--- users/views.py
+++ users/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterForm
 from django.contrib import messages
 # Create your views here.
+
+
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, "Account created successfully")
-            return redirect('users:login')
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account created for {username}!")
+            return redirect('intern_management:details')
     else:
-        form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+        form = UserRegisterForm()
+    return render(request, "users/register.html", {'form': form})
+
+
+def login(request):
+    return render(request, 'users/login.html')
