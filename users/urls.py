28f8ec5723773b128b3a3513494ec24173348497
Luke Lighthart <Cu-Sith@gmx.com>
2019-07-18 15:33:03 -0500

    Added user profile model

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html") , name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html") , name='logout'),
     path('reset/', auth_views.PasswordResetView.as_view(template_name="users/reset.html"), name='reset'),
-
 ]
