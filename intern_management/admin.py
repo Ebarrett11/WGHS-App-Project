25a4e0adbce5f11b41cd00fff5a8febed94c6b43
Luke Lighthart <Cu-Sith@gmx.com>
2019-07-30 20:25:08 -0500

    Edited admin interface. Added footer and style changes

--- intern_management/admin.py
+++ intern_management/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import InternshipLocationModel
 # Register your models here.
+admin.site.site_header = "WGHS Internship Management"
 
-admin.site.register(InternshipLocationModel)
+@admin.register(InternshipLocationModel)
+class InternshipAdmin(admin.ModelAdmin):
+    ordering = ['title']
+    list_display = ('title', 'address', 'contact_email')
+    search_fields = ['title', 'description']
+
+    fieldsets = [
+        (None, {
+            'fields': ('title', 'address')
+        }),
+        ('Description', {
+            'classes': ['wide'],
+            'fields': ('description',)
+        }),
+        ('Students', {
+            'classes': ['wide'],
+            'fields': ('user',)
+        })
+
+    ]
