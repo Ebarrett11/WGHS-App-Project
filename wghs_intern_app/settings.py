ad48172ebe58057b729937ea1d3d874452b124d7
Luke Lighthart <Cu-Sith@gmx.com>
2019-08-08 20:25:44 -0500

    fixed db

--- wghs_intern_app/settings.py
+++ wghs_intern_app/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'wghs_intern_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.sqlite',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
